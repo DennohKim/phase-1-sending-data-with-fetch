810ff77a5efe0b56a253c761d0e5968df4b8e188
Dennis Kimathi <kimathidennis38@gmail.com>
2022-06-17 14:43:15 +0300

    commit tree ef111fcfd03d9198e49bf57b4ea215c66e3e8f9f

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-// Add your code here
+// // Add your code here
+
+// function submitData(name, email){
+//    return fetch('http://localhost:3000/users', {
+//         method: 'POST',
+//         headers: {
+//             'content-type': 'application/json',
+//             Accept: 'application/json',
+//         },
+//         body: JSON.stringify({
+//             name,
+//             email
+//         })
+
+//     })
+//     .then(res => res.json())
+//     .then(data => document.body.innerHTML = data["id"])
+//     .catch(err => document.body.append(err.message))
+//  }
+
+// console.log(submitData("steve", "steve@steve.com"))
 
 function submitData(name, email){
-   return fetch('http://localhost:3000/users', {
-        method: 'POST',
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
         headers: {
-            'content-type': 'application/json',
-            Accept: 'application/json',
+            "Content-Type": "application/json",
+            Accept: "application/json",
         },
         body: JSON.stringify({
-            name: name,
-            email: email
-        })
-
-    })
-    .then(res => res.json())
-    .then(data => document.body.innerHTML = data.id)
-    .catch(err => document.body.append(err.message))
- }
-
-console.log(submitData("steve", "steve@steve.com"))
+            name,
+            email
+        }),
+    }) .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function
+ ( error ) {
+        document.body.innerHTML = error.message
+      } )
+}
